516e2b257d1886fad8bc5f7d862c773457ddc552
danielamissah <daniel.amissah@congressmail.com>
2021-09-13 02:23:11 +0000

    Added Crypto-Js library

---
+++ api/routes/auth.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const User = require("../models/user");
+
+
+//Register
+
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    try {
+        
+        const user = await newUser.save();
+        res.status(201).json(user)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+module.exports = router;
