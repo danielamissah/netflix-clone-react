dcc47a42b59aa48fef6a5d39c1ce33deaeb5c588
danielamissah <daniel.amissah@congressmail.com>
2021-09-13 01:37:25 +0000

    Made connection to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 
+const dotenv = require("dotenv");
 dotenv.config();
 
-connectDB().catch(err => console.log(err)); 
 
-async function connectDB() {
-    await mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
+const port = process.env.PORT;
+const connectionString = process.env.DB_CONNECTION_STRING;
+
+ mongoose.connect(connectionString, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
     })
         .then(() => console.log("DB Connection Success"))
-        .catch((err) => console.log(err));
-}
+        .catch((err) => console.log("Unable to connect to DB: " + err));
 
-app.listen(8800, () => {
+app.listen(port, () => {
     console.log("Backend Server is running");
 });
