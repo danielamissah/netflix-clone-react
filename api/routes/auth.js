53384c237108f3e0525a52aa81ebb3a87abb0dc2
danielamissah <daniel.amissah@congressmail.com>
2021-09-13 02:30:09 +0000

    Added Registration Function

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const User = require("../models/user");
 
+const CryptoJS = require("crypto-js");
+
 
 //Register
 
@@ -9,7 +11,7 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
     });
 
     try {
