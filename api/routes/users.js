99278832a5d9f32d2b2a57e93251afc16c05d7aa
danielamissah <daniel.amissah@congressmail.com>
2021-09-16 02:20:14 +0000

    Added Update, Delete, Get all Users functionality

---
+++ api/routes/users.js
@@ -0,0 +1,85 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const verify = require('../verifyToken');
+const user = require("../models/User");
+
+//UPDATE
+router.put("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        if (req.user.password) {
+            req.body.password = CryptoJS.AES.encrypt(
+                req.body.password, process.env.SECRET_KEY).toString();
+            
+        }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id, {
+                $set: req.body
+            }, {
+                new: true
+            });
+            res.status(200).json(updatedUser);
+            
+        } catch (err) {
+            res.status(500).json(err);
+            
+        }
+    } else {
+        res.status(403).json("You can update only your account");
+    }
+})
+
+
+//DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted");
+            
+        } catch (err) {
+            res.status(500).json(err);
+            
+        }
+    } else {
+        res.status(403).json("You can delete only your account");
+    }
+})
+
+//GET USER
+router.get("/find/:id", async (req, res) => {
+        
+        try {
+            const user = await User.findById(req.params.id);
+            const { password, ...info } = user._doc;
+            res.status(200).json(info);
+            
+        } catch (err) {
+            res.status(500).json(err);
+            
+        }
+    }
+)
+
+//GET ALL USERS
+router.get("/", verify, async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        
+        try {
+            const users = query ? await User.find().sort({_id:-1}).limit(10) : await User.find();
+            res.status(200).json(users);
+            
+        } catch (err) {
+            res.status(500).json(err);
+            
+        }
+    } else {
+        res.status(403).json("You're not an admin");
+    }
+})
+//GET USER STATS
+
+module.exports = router;
