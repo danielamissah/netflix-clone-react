69df39211ba8b55f43eb7f49e6780058d3c8e1aa
Emmanuel Ametepee <49801256+EmmanuelAmet@users.noreply.github.com>
2021-09-16 22:34:50 -0700

    user login endpoint fix and tested successfully

--- api/index.js
+++ api/index.js
@@ -8,10 +8,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 
-const port = process.env.PORT;
+const port = 3001//process.env.PORT;
 const connectionString = process.env.DB_CONNECTION_STRING;
 
- mongoose.connect(connectionString, {
+ mongoose.connect('mongodb+srv://emmanuel:Emmanuel123@netflix-cluster.pxngm.mongodb.net/netflix?retryWrites=true&w=majority', {
      useNewUrlParser: true,
      useUnifiedTopology: true,
     })
@@ -19,8 +19,8 @@ const connectionString = process.env.DB_CONNECTION_STRING;
     .catch((err) => console.log("Unable to connect to DB: " + err));
         
 app.use(express.json())
-app.use("/api/routes/auth", authRoute);
-app.use("/api/routes/users", userRoute);
+app.use(authRoute);
+app.use(userRoute);
 
 app.listen(port, () => {
     console.log("Backend Server is running");
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -25,7 +25,7 @@ router.post("/register", async (req, res) => {
 
 //Login
 
-router.post("login", async (req, res) => {
+router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email });
         !user && res.status(401).json("Invalid User name or password");
