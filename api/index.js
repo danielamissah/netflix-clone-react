5d1e709a4064367bac6a8acd8688562b12926d13
danielamissah <daniel.amissah@congressmail.com>
2021-09-23 03:37:18 +0000

    API done

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -21,6 +23,8 @@ const connectionString = process.env.DB_CONNECTION_STRING;
 app.use(express.json())
 app.use("/api/routes/auth", authRoute);
 app.use("/api/routes/users", userRoute);
+app.use("/api/routes/movies", movieRoute);
+app.use("/api/routes/lists", listRoute);
 
 app.listen(port, () => {
     console.log("Backend Server is running");
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -5,70 +5,70 @@ const Movie = require("../models/movie")
 
 //CREATE A NEW MOVIE
 router.post("/", verify, async (req, res) => {
-        if (req.user.isAdmin) {
-            const newMovie = new Movie(req.body);
+    if (req.user.isAdmin) {
+        const newMovie = new Movie(req.body);
 
-            try {
-                const savedMovie = await newMovie.save();
-                res.status(201).json(savedMovie);
-            } catch (err) {
-                res.status(500).json(err);
-            }
+        try {
+            const savedMovie = await newMovie.save();
+            res.status(201).json(savedMovie);
+        } catch (err) {
+            res.status(500).json(err);
         }
+    }
         
-     else {
+    else {
         res.status(403).json("You don't have permissions to do that");
     }
-})
+});
 
 //UPDATE A MOVIE
 router.put("/:id", verify, async (req, res) => {
-        if (req.user.isAdmin) {
-            try {
-                const updatedMovie = await Movie.findByIdAndUpdate(req.params.id,
-                    { $set: req.body,}, {
-                    new: true
-                });
-                res.status(200).json(updatedMovie);
-            } catch (err) {
-                res.status(500).json(err);
-            }
+    if (req.user.isAdmin) {
+        try {
+            const updatedMovie = await Movie.findByIdAndUpdate(req.params.id,
+                { $set: req.body, }, {
+                new: true
+            });
+            res.status(200).json(updatedMovie);
+        } catch (err) {
+            res.status(500).json(err);
         }
+    }
         
-     else {
+    else {
         res.status(403).json("You don't have permissions to do that");
     }
-})
+});
 
 
 //DELETE A MOVIE
 router.delete("/:id", verify, async (req, res) => {
-        if (req.user.isAdmin) {
-            try {
-                 await Movie.findByIdAndDelete(req.params.id);
-                res.status(200).json("The movie has been deleted");
-            } catch (err) {
-                res.status(500).json(err);
-            }
+    if (req.user.isAdmin) {
+        try {
+            await Movie.findByIdAndDelete(req.params.id);
+            res.status(200).json("The movie has been deleted");
+        } catch (err) {
+            res.status(500).json(err);
         }
+    }
         
-     else {
+    else {
         res.status(403).json("You don't have permissions to do that");
     }
-})
+});
 
 //GET A MOVIE
-router.delete("/:id", verify, async (req, res) => {
-            try {
-             const movie =  await Movie.findById(req.params.id);
-                res.status(200).json(movie);
-            } catch (err) {
-                res.status(500).json(err);
-            }
-})
+router.get("/find/:id", verify, async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        res.status(200).json(movie);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //GET A RANDOM MOVIE
-router.delete("/random", verify, async (req, res) => {
+router.get("/random", verify, async (req, res) => {
     const type = req.query.type;
     let movie;
     try {
@@ -77,7 +77,7 @@ router.delete("/random", verify, async (req, res) => {
                 { $match: { isSeries: true } },
                 { $sample: { size: 1 } }
             ]);
-                }
+        }
         else {
             movie = await Movie.aggregate([
                 { $match: { isSeries: false } },
@@ -86,12 +86,26 @@ router.delete("/random", verify, async (req, res) => {
         }
         res.status(200).json(movie);
 
-            } catch (err) {
-                res.status(500).json(err);
-            }
-        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET ALL MOVIES
+router.get("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const movies = await Movie.find();
+            res.status(200).json(movies.reverse());
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+        
+    else {
+        res.status(403).json("You don't have permissions to do that");
+    }
+});
 
 module.exports = router;
 
-
-//got to 1:23:10
